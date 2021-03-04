1978a6e816e5b4093d0a1c3e7890acd955ce80d9
bamjun <khy5116@naver.com>
2021-03-04 19:35:27 +0900

    Create 10진수->n진수 변환.py

---
+++ 10진수->n진수 변환.py
@@ -0,0 +1,17 @@
+def solution(n, jinsu):
+    a = ''
+    while n :
+        n, b = divmod(n, jinsu)
+        a += str(b)
+    return a[::-1]
+
+
+n = 125
+#십진수
+ss = 3
+#변환진수
+print(solution(n, ss))
+
+
+
+
